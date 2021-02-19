5e065a4e6dce315e51f89a9fd1f3c4a7f08bd261
restevean <restevean@protonmail.com>
2021-02-19 20:58:08 +0100

    "Archivo leído correctamente y contenido cargado en una lista"

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ con el resultado del proceso
 
 import csv
 
-with open("file.csv", newline='', encoding='utf-16') as csvfile:
-    reader = csv.reader(csvfile)
-    for fileLine in reader:
-        print(fileLine)
+stop=0
+
+# Abrimos el archivo .csv
+with open("file.csv", encoding='utf-16') as csvfile:
+    # Lo leemos con csv.Reader()
+    contenidoCsv = csv.reader(csvfile, dialect='excel-tab')
+    # Cargamos el contenido leído en la lista
+    listaDeAsistencia = list(contenidoCsv)
+    print('==============Lista==============')
+    # Imprimimos la lista
+    for evento in listaDeAsistencia:
+        print(evento)
+
+print("Done ;)")
+
+# print("==============Segundo print==============")
+# with open("file.csv", encoding='utf-16') as csvfile:
+    # lo leemos con csv.DictReader()
+    #     dict_reader = csv.DictReader(csvfile, dialect='excel-tab')
+    # for element in dict_reader:
+    #   print(element['Nombre completo'], element['Acción del usuario'], element['Marca de tiempo'])
