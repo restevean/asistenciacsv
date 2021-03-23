92cb42d93acfa1ffde1c2123647cd648ccd49516
restevean <restevean@protonmail.com>
2021-03-23 14:27:45 +0100

    Funcionando por primera vez

--- main.py
+++ main.py
@@ -5,65 +5,107 @@ con el resultado del proceso
 """
 
 import csv
+from csv import reader as readcsv
 from datetime import datetime
 
-listaNueva = [['Nombre completo', 'Acción del usuario', 'Marca de tiempo']]
-inicioReunion = 0
-finalReunion = 'Final'
 
-# Abrimos el archivo .csv
-print('Reading csv file...')
-with open("fixtures/file0.csv", encoding='utf-16') as csvfile:
-    # Lo leemos con csv.Reader()
-    contenidoCsv = csv.reader(csvfile, dialect='excel-tab')
+def parse(contenido_csv_param):
+    """
+    :type contenido_csv_param: list
+    """
+    finalreuniont = 'Final'
+    lend = False  # Asistente no tiene linea de final
 
-    # Omitimos el encabezado del archivo .csv
-    next(contenidoCsv)
-
+    # Procesamos el encabezado de archivo.csv
+    cabecera = next(contenido_csv_param)
     # Cargamos el contenido leído en la lista
-    listaDeAsistencia = list(contenidoCsv)
+    listadeasistencia = list(contenido_csv_param)
+    # Asignamos la cabecera a la primera linea de la nueva lista
+    listanueva = [cabecera]
+    listanueva[0].append('T. Efectivo')
+    # Append first row
+    listadeasistencia[0][2] = datetime.strptime(listadeasistencia[0][2], '%d/%m/%Y %H:%M:%S')
+    listanueva.append(listadeasistencia[0])
+    listanueva[1].append('')
+    # Asignamos las variables de inicio reunión
+    inicioreunion = listanueva[1][2]
+    finalreunion = listanueva[1][2]
+    t1 = listanueva[1][2]
 
     print('Parsing data...')
 
     # Procesamos la lista
-    for evento in listaDeAsistencia:
+    # 1ª vuelta
+    for linea in listadeasistencia:
+        if type(linea[2]) == str:
+            linea[2] = datetime.strptime(linea[2], '%d/%m/%Y %H:%M:%S')
 
-        # Convertimos el último elemento en datetime
-        evento[2] = datetime.strptime(evento[2], '%d/%m/%Y %H:%M:%S')
+        if linea[0] != listanueva[len(listanueva) - 1][0]:
+            if listanueva[len(listanueva) - 1][1] == 'Unido':
+                listanueva.append([listanueva[len(listanueva) - 1][0], 'Abandonó', finalreuniont, ''])
+                lend = True
 
-        # Si no hay inicio de reunión, asignamos el tiempo de inicio de la reunión
-        if inicioReunion == 0:
-            inicioReunion = evento[2]
+            if linea[1] == 'Se unió antes':
+                listanueva.append([linea[0], "Unido", inicioreunion, ''])
+                lend = False
+            elif linea[1] == 'Unido':
+                listanueva.append([linea[0], linea[1], linea[2], ''])
+                lend = False
+            elif linea[1] == 'Abandonó':
+                listanueva.append([linea[0], "Unido", inicioreunion, ''])
+                listanueva.append([linea[0], "Abandonó", linea[2], ''])
+                lend = True
 
-        # Tomamos la cabecera
-        cabeceraArchivo = listaDeAsistencia[0]
-        # Asignamos el nombre
-        nombreAsistente = evento[0]
-        # Asignamos la acción
-        accion = evento[1]
-        # asignamos el tiempo de inicio de la reunión
-        tiempoInicioReunion = evento[2]
+        elif linea[0] == listanueva[len(listanueva) - 1][0]:
+            if linea[1] == 'Unido':
+                lend = False
+            elif linea[1] == 'Abandonó':
+                if listanueva[len(listanueva) - 1][1] == 'Unido':
+                    if linea[1] == 'Abandonó':
+                        listanueva.append([linea[0], linea[1], linea[2], ''])
+                        lend = True
+                    elif linea[1] == 'Unido':
+                        lend = False
+                elif listanueva[len(listanueva) - 1][1] == 'Abandonó':
+                    listanueva[len(listanueva) - 1][2] = linea[2]
+                    lend = True
 
-        # Si el nombre anterior es distinto
-        if nombreAsistente != listaNueva[len(listaNueva) - 1][0]:
-            # Si la acción == Se unió antes
-            if evento[1] == 'Se unió antes':
-                evento[1] = 'Unido'
-                evento[2] = tiempoInicioReunion
-                listaNueva.append([evento[0], "Unido", TI, ''])
-            # Si la acción == 'Unido'
-            elif evento[1] == 'Unido':
-                if listaNueva[len(listaNueva) - 1][1] == 'Unido':
-                    listaNueva.append(
-                        [listaNueva[len(listaNueva) - 1][0], 'Abandonó', listaNueva[len(listaNueva) - 1][2], 'TF'])
-                listaNueva.append([evento[0], evento[1], evento[2], ''])
-            # Si la acción == 'Abandonó'
-            elif evento[1] == 'Abandonó':
-                listaNueva.append([evento[0], 'Unido', TI, ''])
-                listaNueva.append([evento[0], 'Abandonó', evento[2], ''])
-        if nombreAsistente == listaNueva[len(listaNueva) - 1]:
-            if evento[1] == 'Abandonó':
-                listaNueva.append([evento[0], evento[1], evento[2], ''])
+        if finalreunion < linea[2]:
+            finalreunion = linea[2]
+
+    if not lend:
+        if listanueva[len(listanueva) - 1][1] == 'Unido':
+            listanueva.append([listanueva[len(listanueva) - 1][0], 'Abandonó', finalreuniont, ''])
+        elif listanueva[len(listanueva) - 1][1] == 'Abandonó':
+            listanueva[len(listanueva) - 1][2] = finalreuniont
+
+    # 2ª vuelta
+    for lineaN in listanueva:
+        # Si no tiene tiempo de inicio le asignamos el inicio de la reunión
+        if lineaN[2] == 'Inicio':
+            lineaN[2] = inicioreunion
+
+        # Si no tiene tiempo de fin, le asignamos el final de la reunión
+        elif lineaN[2] == 'Final':
+            lineaN[2] = finalreunion
+
+        if lineaN[1] == 'Unido':
+            t1 = lineaN[2]
+        elif lineaN[1] == 'Abandonó':
+            t2 = lineaN[2]
+            # Calculamos el tiempo efectivo desde que entró hasta que salió
+            pepe = t2 - t1
+            lineaN[3] = pepe
+
+    return listanueva
+
+
+# Abrimos el archivo.csv
+print('Reading csv file...')
+with open("fixtures/2021-02-25.csv", encoding='utf-16') as csvfile:
+    # Lo leemos con csv.Reader()
+    contenidocsv: readcsv = csv.reader(csvfile, dialect='excel-tab')
+    parsedlist = parse(contenidocsv)  # Recibimos la lista procesada
 
 # Escribimos listaNueva en un archivo .csv delimitado por tabulaciones
 with open("output.csv", 'w', encoding='utf-16') as csvFileOutput:
@@ -72,13 +114,7 @@ with open("output.csv", 'w', encoding='utf-16') as csvFileOutput:
 
     print('Writting csv file: output.csv')
     # Escribimos todas las líneas
-    for eventoN in listaNueva:
-        contenidoNuevo.writerow(eventoN)
+    for evento in parsedlist:
+        contenidoNuevo.writerow(evento)
 
 print("Done ;)")
-
-
-def parse(contenidoCsv):
-
-
-    return
